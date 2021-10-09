650d07dafa76426a07919c2c3de27264a1baaa2f
alyssandracordero <alyssandraskittles@gmail.com>
2021-10-09 00:56:48 -0600

    test in dev

--- Assignments/Lab_Assignments/Lab2/riscv.c
+++ Assignments/Lab_Assignments/Lab2/riscv.c
@@ -75,13 +75,7 @@ char* rm_first_char(char* original){
 // value. multiply res by 10 to shuffle digits left.
 // For each ASCII character of the digit: subtract the code from '0' to get numerical value.
 
-/**
- * Fill out this function and use it to read interpret user input to execute RV64 instructions.
- * You may expect that a single, properly formatted RISC-V instruction string will be passed
- * as a parameter to this function.
- */
 //instr is user input string
-
 bool interpret(char* instr){
 	int suk;
 	bool successful_op = false; 
@@ -202,46 +196,30 @@ void write_read_demo(){
 }
 
 /**
- * Your code goes in the main
- *
+ * main()
  */
 
 int main(){
+	
 	// Do not write any code between init_regs
 	init_regs(); // DO NOT REMOVE THIS LINE
 	print_regs();
+	
+	
 	// Below is a sample program to a write-read. Overwrite this with your own code.
 	printf(" RV32 Interpreter.\nType RV32 instructions. Use upper-case letters and space as a delimiter.\nEnter 'EOF' character to end program\n");
-	
 	input = malloc(1000 * sizeof(char));//allocate memory for input
-	
-	bool is_null = false;
-	//fgets() returns null if EOF is reached.
-		//while(!is_null){
-	//	interpret(input);
 
-	//is_null = fgets(input, 1000, stdin) == NULL;
-	//}
-	//fgets(input, input_size, stdin);
-	//bool is_done = false;  
-	while(!is_null && !is_done){//while input is NOT NULL
-//		printf("HEELLO!\n%s\n", *input);
-		//char* eof = "EOF";
-		//if(equals(eof, *input)){
-	//	//printf("ESTOY EN EOF!");
-		//	is_done = true;
-		//}else{
-		interpret(input);//if instruction exists
+	bool is_null = false;
+	
+	while(!is_null && !is_done){
+		interpret(input);
 		printf("\n");
 		print_regs();
-		printf("\n");	
+		printf("\n");
 		is_null = fgets(input, 1000, stdin) == NULL;
-		//}
-		//else{
-	//	printf("ESTOY EN SIN INPUT\n");
-		//}
 	}
 	printf("Good bye!\n");
 	free(input);//free input
 	return 0;
-}
+}//END main()!!
